4ba62ef31e60c7dce5f4f936d5aeb95159bfd1e2
Nicola Zoboli <64474850+NicolaZoboli@users.noreply.github.com>
2020-11-24 19:10:22 +0100

    Creato controller con cUrl per ottenere array orders

---
+++ MagentoApiRest/app/Http/Controllers/OrderController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Ixudra\Curl\Facades\Curl;
+
+class OrderController extends Controller
+{
+  public function getData()
+  {
+      $getToken = Curl::to('https://web-web-fri.gdl.glamdigitallab.it/rest/V1/integration/admin/token?username=friclassroom&password=fri123456')
+              ->post();
+
+      $token = json_decode($getToken);
+
+
+      $getOrders = Curl::to('https://web-web-fri.gdl.glamdigitallab.it/rest/V1/orders?searchCriteria=all')
+              ->withBearer($token)
+              ->get();
+
+      $orders = json_decode($getOrders);
+
+      dd($orders);
+  }
+}
